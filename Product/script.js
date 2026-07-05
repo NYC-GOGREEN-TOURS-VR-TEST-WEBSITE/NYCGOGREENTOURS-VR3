0930b55674fb81d8862804b830ef174273289def
GoGreenTours Workspace. <brandgroupgogreen@gmail.com>
2026-07-05 08:10:16 -0400

    Add files via upload

---
+++ Product/script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for your inquiry. NYC GoGreen Tours will contact you shortly.");
+});
