215ae8fb25deaebf191ec320f809abca2d476ed8
GoGreenTours Workspace. <brandgroupgogreen@gmail.com>
2026-07-05 09:00:22 -0400

    Delete Product directory

--- Product/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Thank you for your inquiry. NYC GoGreen Tours will contact you shortly.");
-});
